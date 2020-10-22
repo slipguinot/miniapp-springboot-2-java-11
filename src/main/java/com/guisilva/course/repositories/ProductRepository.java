c9304c790d4875e0b31524f9853aad384e2e2e15
Guilherme Silva <gdssilva@id.uff.br>
2020-10-22 11:41:06 -0300

    Adicionada entidade, recurso, servico Product

---
+++ src/main/java/com/guisilva/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.guisilva.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guisilva.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
